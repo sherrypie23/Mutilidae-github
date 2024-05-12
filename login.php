2b985fc4d9c035ad77797372fbdf77d474a2bfad
sherrypie23 <167986935+sherrypie23@users.noreply.github.com>
2024-05-12 12:24:36 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
-<?php 
-session_start();
-	try {	    	
+<?php
+	try {
     	switch ($_SESSION["security-level"]){
     		case "0": // This code is insecure.
 				$lEnableJavaScriptValidation = FALSE;
@@ -8,10 +7,6 @@ session_start();
 			break;
 
     		case "1": // This code is insecure.
-				$lEnableJavaScriptValidation = TRUE;
-				$lEnableHTMLControls = TRUE;
-			break;
-
 	   		case "2":
 	   		case "3":
 	   		case "4":
@@ -20,51 +15,49 @@ session_start();
 				$lEnableHTMLControls = TRUE;
     		break;
     	}// end switch
-    	
+
 	} catch(Exception $e){
 		echo $CustomErrorHandler->FormatError($e, "Error setting up configuration.");
-	}// end try	
+	}// end try
 ?>
 
 <script type="text/javascript">
 <!--
-	<?php 
+	<?php
 		if ($_SESSION["loggedin"]=="True") {
 			echo "var l_loggedIn = true;" . PHP_EOL;
 		}else {
 			echo "var l_loggedIn = false;" . PHP_EOL;
 		}// end if
-	
+
 		if (isset($lAuthenticationAttemptResult)){
 			echo "var lAuthenticationAttemptResultFlag = {$lAuthenticationAttemptResult};" . PHP_EOL;
 		}else{
 			echo "var lAuthenticationAttemptResultFlag = -1;".PHP_EOL;
 		}// end if
- 
+
 		if($lEnableJavaScriptValidation){
 			echo "var lValidateInput = \"TRUE\"" . PHP_EOL;
 		}else{
 			echo "var lValidateInput = \"FALSE\"" . PHP_EOL;
-		}// end if		
+		}// end if
 	?>
 
 	function onSubmitOfLoginForm(/*HTMLFormElement*/ theForm){
 		try{
 			if(lValidateInput == "TRUE"){
-				var lUnsafeCharacters = /[`~!@#$%^&*()-_=+\[\]{}\\|;':",./<>?]/;
-				if (theForm.username.value.length > 15 || 
-					theForm.password.value.length > 15){
+				var lUnsafeCharacters = /[\W]/g;
+				if (theForm.username.value.length > 15){
 						alert('Username too long. We dont want to allow too many characters.\n\nSomeone might have enough room to enter a hack attempt.');
 						return false;
 				};// end if
-				
-				if (theForm.username.value.search(lUnsafeCharacters) > -1 || 
-					theForm.password.value.search(lUnsafeCharacters) > -1){
+
+				if (theForm.username.value.search(lUnsafeCharacters) > -1){
 						alert('Dangerous characters detected. We can\'t allow these. This all powerful blacklist will stop such attempts.\n\nMuch like padlocks, filtering cannot be defeated.\n\nBlacklisting is l33t like l33tspeak.');
 						return false;
 				};// end if
 			};// end if(lValidateInput)
-			
+
 			return true;
 		}catch(e){
 			alert("Error: " + e.message);
@@ -73,38 +66,18 @@ session_start();
 //-->
 </script>
 
-<!-- Bubble hints code -->
-<?php 
-	try{
-   		$lReflectedXSSExecutionPointBallonTip = $BubbleHintHandler->getHint("ReflectedXSSExecutionPoint");
-   		$lSQLInjectionPointBallonTip = $BubbleHintHandler->getHint("SQLInjectionPoint");
-   		
-	} catch (Exception $e) {
-		echo $CustomErrorHandler->FormatError($e, "Error attempting to execute query to fetch bubble hints.");
-	}// end try
-?>
-
-<script type="text/javascript">
-	$(function() {
-		$('[ReflectedXSSExecutionPoint]').attr("title", "<?php echo $lReflectedXSSExecutionPointBallonTip; ?>");
-		$('[ReflectedXSSExecutionPoint]').balloon();
-		$('[SQLInjectionPoint]').attr("title", "<?php echo $lSQLInjectionPointBallonTip; ?>");
-		$('[SQLInjectionPoint]').balloon();		
-	});
-</script>
-
 <div class="page-title">Login</div>
 
-<?php include_once (__ROOT__.'/includes/back-button.inc');?>
-<?php include_once (__ROOT__.'/includes/hints/hints-menu-wrapper.inc'); ?>
+<?php include_once (__SITE_ROOT__.'/includes/back-button.inc');?>
+<?php include_once (__SITE_ROOT__.'/includes/hints/hints-menu-wrapper.inc'); ?>
 
 <div id="id-log-in-form-div" style="display: none; text-align:center;">
 	<form 	action="index.php?page=login.php"
-			method="post" 
-			enctype="application/x-www-form-urlencoded" 
+			method="post"
+			enctype="application/x-www-form-urlencoded"
 			onsubmit="return onSubmitOfLoginForm(this);"
 			id="idLoginForm">
-		<table style="margin-left:auto; margin-right:auto;">
+		<table>
 			<tr id="id-authentication-failed-tr" style="display: none;">
 				<td id="id-authentication-failed-td" colspan="2" class="error-message"></td>
 			</tr>
@@ -116,7 +89,7 @@ session_start();
 			<tr>
 				<td class="label">Username</td>
 				<td>
-					<input	SQLInjectionPoint="1" type="text" name="username" size="20"
+					<input	type="text" name="username" size="20"
 							autofocus="autofocus"
 					<?php
 						if ($lEnableHTMLControls) {
@@ -129,7 +102,7 @@ session_start();
 			<tr>
 				<td class="label">Password</td>
 				<td>
-					<input SQLInjectionPoint="1" type="password" name="password" size="20"
+					<input type="password" name="password" size="20"
 					<?php
 						if ($lEnableHTMLControls) {
 							echo('minlength="1" maxlength="15" required="required"');
@@ -157,7 +130,7 @@ session_start();
 <div id="id-log-out-div" style="text-align: center; display: none;">
 	<table>
 		<tr>
-			<td ReflectedXSSExecutionPoint="1" colspan="2" class="hint-header">You are logged in as <?php echo $_SESSION['logged_in_user']; ?></td>
+			<td colspan="2" class="hint-header">You are logged in as <?php echo $_SESSION['logged_in_user']; ?></td>
 		</tr>
 		<tr><td></td></tr>
 		<tr><td></td></tr>
@@ -166,7 +139,7 @@ session_start();
 				<input class="button" type="button" value="Logout" onclick="document.location='index.php?do=logout'" />
 			</td>
 		</tr>
-	</table>	
+	</table>
 </div>
 
 <script type="text/javascript">
@@ -177,25 +150,25 @@ session_start();
    	var cAUTHENTICATION_SUCCESSFUL = 3;
    	var cAUTHENTICATION_EXCEPTION_OCCURED = 4;
    	var cUSERNAME_OR_PASSWORD_INCORRECT = 5;
-   	
+
    	var lMessage = "";
    	var lAuthenticationFailed = "FALSE";
-   	
+
 	switch(lAuthenticationAttemptResultFlag){
-   		case cACCOUNT_DOES_NOT_EXIST: 
+   		case cACCOUNT_DOES_NOT_EXIST:
    	   		lMessage="Account does not exist"; lAuthenticationFailed = "TRUE";
    	   		break;
-   		case cPASSWORD_INCORRECT: 
-   	   		lMessage="Password incorrect"; lAuthenticationFailed = "TRUE"; 
+   		case cPASSWORD_INCORRECT:
+   	   		lMessage="Password incorrect"; lAuthenticationFailed = "TRUE";
    	   		break;
-   		case cNO_RESULTS_FOUND: 
-   	   		lMessage="No results found"; lAuthenticationFailed = "TRUE"; 
+   		case cNO_RESULTS_FOUND:
+   	   		lMessage="No results found"; lAuthenticationFailed = "TRUE";
    	   		break;
-   		case cAUTHENTICATION_EXCEPTION_OCCURED: 
-   	   		lMessage="Exception occurred"; lAuthenticationFailed = "TRUE"; 
+   		case cAUTHENTICATION_EXCEPTION_OCCURED:
+   	   		lMessage="Exception occurred"; lAuthenticationFailed = "TRUE";
    		break;
-   		case cUSERNAME_OR_PASSWORD_INCORRECT: 
-   	   		lMessage="Username or password incorrect"; lAuthenticationFailed = "TRUE"; 
+   		case cUSERNAME_OR_PASSWORD_INCORRECT:
+   	   		lMessage="Username or password incorrect"; lAuthenticationFailed = "TRUE";
    		break;
    	};
 
@@ -209,6 +182,6 @@ session_start();
 		document.getElementById("id-log-out-div").style.display="none";
 	}else{
 		document.getElementById("id-log-in-form-div").style.display="none";
-		document.getElementById("id-log-out-div").style.display="";		
-	}// end if l_loggedIn	
+		document.getElementById("id-log-out-div").style.display="";
+	}// end if l_loggedIn
 </script>
